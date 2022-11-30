e9643e1a9e58b31b40fc625a80adb41aad46e0f9
Anurag Jha <theunknownlol049@gmail.com>
2022-11-30 14:04:42 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Enter your name: ";
+    string name;
+    cin >> name;
+    cout << "thanks a lot " << name << " for checking out my repository... I will appreciate if you can contribute something..\n";
+    cout << "hope to see you here again..";
+
+
 }
